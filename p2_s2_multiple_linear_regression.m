5c32670a5206fc70668aa4d6ec54566e0138a099
umbraclet16 <448101470@qq.com>
2016-09-18 22:13:41 +0800

    P2: Achieve multiple linear regression using 800 samples and test the model using 200 samples. Save results to mat files.
    Regression methods are regress() and robustfit()[logistic].
    TODO: remove stepwise().

--- p2_s2_multiple_linear_regression.m
+++ p2_s2_multiple_linear_regression.m
@@ -5,14 +5,19 @@
 use_genotype_3x = 1;
 % Choose multiple linear regression method.
 % 1: regress; 2: stepwisefit; 3: robustfit(logistic).
-reg_method = 3;
+% TODO: REMOVE 2!!!
+reg_method = 1;
+% Threshold(p) in chi-square test.
+% Takes value in [0.01;0.001;0.0001]
+threshold = 0.01;
 %------------------------------------------------------------
 % 1: chi-square test; 2: infinite norm.
 p2_extract_method = 1;
+% Save result to .mat file?
+p2_save_result = 1;
 
 %%
 % Load possible_pathogenic_idx from .mat file.
-global threshold
 if p2_extract_method == 1   % chi-square test.
     str1 = 'chi2';
     switch threshold
@@ -32,12 +37,13 @@ end
 
 mat_name_str = ['p2_' str1 '_pathogenic_idx_3x_p' str2 '.mat'];
 
-if ~exist('possible_pathogenic_idx','var')
+% ALWAYS load data from file!!!
+% if ~exist('possible_pathogenic_idx','var')
     load(mat_name_str)
     fprintf('Loading from %s...\n',mat_name_str)
-end
+% end
 
-clear p2_extract_method str1 str2 mat_name_str
+clear p2_extract_method mat_name_str
 
 %%
 switch reg_method
@@ -48,7 +54,6 @@ switch reg_method
     case 3
         reg = 0; stepwise_reg = 0; logistic = 1;
 end
-clear reg_method
 
 %%
 % Multiple linear regression analysis on the 
@@ -60,6 +65,10 @@ clear reg_method
 % X: most distinct n columns in genotype_3x corresponding to each of the 
 %       possible pathogenic sites obtained above.
 Y = phenotype;
+% Remove 200 testing samples. Cannot change order of next 2 lines
+% because it causes 'Matrix index is out of range for deletion.'
+Y(901:1000,:) = [];
+Y(401:500,:) = [];
 X = [];
 
 if use_genotype_3x
@@ -75,6 +84,9 @@ for i = 1 : length(possible_pathogenic_idx)
 end
 %------------------------------------------------------------
 end
+% Remove 200 testing samples.
+X(901:1000,:) = [];
+X(401:500,:) = [];
 
 %%
 if reg
@@ -82,7 +94,7 @@ if reg
 % B = regress(Y,X) returns the vector B of regression coefficients
 % in the linear model Y = X * B.
 % NOTICE: must add constant term to the left of X!
-X = [ones(num_samples,1),X];    % add constant term to X.
+X = [ones(num_samples - 200,1),X];    % add constant term to X.
 [B,BINT,R,RINT,STATS] = regress(Y,X);
 
 STATS
@@ -93,8 +105,9 @@ if STATS(1) >= 0.95     % R-square > 0.95
     fprintf('Multiple linear fitting succeeded!\n');
 end
 
-Yhat = X*B;
+Yhat = X * B;
 disp('Regress fit finished.')
+clear BINT R RINT % Don't need them now.
 %------------------------------------------------------------
 end
 %%
@@ -107,6 +120,7 @@ PREMOVE = 0.10;
 
 Yhat = X * B;
 disp('Stepwise fit finished.')
+clear SE PVAL INMODEL NEXTSTEP HISTORY % Don't need them now.
 %------------------------------------------------------------
 end
 
@@ -114,7 +128,7 @@ end
 if logistic
 %------------------------------------------------------------
 [B,STATS] = robustfit(X,Y,'logistic');
-Yhat = [ones(num_samples,1) X] * B;
+Yhat = [ones(num_samples - 200,1) X] * B;
 disp('Logistic fit finished.')
 
 % for i = 1 : 5
@@ -131,4 +145,31 @@ end
 %%
 % Calculate accuracy of discrimination using 200 test samples.
 p2_s3_calc_correct_pct;
+
+%%
+% Save to .mat file.
+if p2_save_result
+%------------------------------------------------------------
+% str1:extract_method; str2: threshold
+switch reg_method
+    case 1
+        str_method = '_reg';
+    case 2
+        str_method  = '_stepwise';
+    case 3
+        str_method = '_logistic';
+end
+filename = ['p2_result_' str1 str_method str2 '.mat'];
+save(filename,'threshold','B','healthy_test','healthy_test_correct_pct',...
+    'ill_test','ill_test_correct_pct','healthy_training','healthy_training_correct_pct',...
+    'ill_training','ill_training_correct_pct','sorted_psb','sorted_psb_idx','STATS')
+
+fprintf('Save to file %s.\n',filename)
+%------------------------------------------------------------
+end
+clear p2_save_result
+
+%%
+clear str1 str2 filename str_method reg_method
+
 
