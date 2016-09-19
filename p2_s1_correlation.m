50ba531d9afcdbe2b2e136092420887c5de93891
umbraclet16 <448101470@qq.com>
2016-09-19 19:15:00 +0800

    P2: s1_correlation. Remove useless code.

--- p2_s1_correlation.m
+++ p2_s1_correlation.m
@@ -3,14 +3,10 @@
 
 %%
 training_samples = genotype_3x;
-% training_samples(701:1000,:) = [];
-% training_samples(201:500,:) = [];
 training_samples(901:1000,:) = [];
 training_samples(401:500,:) = [];
 X  = training_samples;
 Y = phenotype;
-% Y(701:1000,:) = [];
-% Y(201:500,:) = [];
 Y(901:1000,:) = [];
 Y(401:500,:) = [];
 
@@ -20,3 +16,4 @@ correlation = corr(X,Y);
 possible_pathogenic_idx = sorted_correlation_idx(1:200);
 possible_pathogenic_idx = sort(possible_pathogenic_idx,'ascend');
 
+sorted_psb_idx = floor((sorted_correlation_idx - 1) / 3) + 1;
