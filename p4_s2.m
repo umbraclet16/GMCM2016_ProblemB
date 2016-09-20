babc0dbad04931b8cbf1d7fd9acdc650e000902a
umbraclet16 <448101470@qq.com>
2016-09-20 09:22:26 +0800

    P4: s2: pick samples satisfying sum(multi_phenos(i,:))=10 or 0. Then y is transformed from a [1000*10] matrix to [xx*1]vector, we can apply the model in P2.

--- p4_s2.m
+++ p4_s2.m
@@ -2,46 +2,47 @@
 %
 iterate = 0;
 min_coef = 5;
+num_psb_sites = 286; % p = 0.001: 28;  p = 0.01: 286; p = 0.05: 1481.
 
 %%
-% Use all samples. Keep no samples for test.
-x = genotype_3x;
-y = multi_phenos; % [1000*10]
-% Extract top 150 sites for each character(性状).
-num_sites_each_character = 150;
-
-% Use to save 150*10=1500 possible pathogenic sites(bits).
-% There could be repetitions! Do we need to deal with it? Guess so...
-possible_pathogenic_idx = zeros(num_sites_each_character,num_characters);
-
-for i = 1 : num_characters
-    
-correlation = corr(x,y(:,i));
-[sorted_correlation,sorted_correlation_idx] = sort(abs(correlation),'descend');
-
-possible_pathogenic_idx(:,i) = sorted_correlation_idx(1:num_sites_each_character);
-% possible_pathogenic_idx(:,i) = sort(possible_pathogenic_idx(:,i),'ascend');
-
-% sorted_psb_idx = floor((sorted_correlation_idx - 1) / 3) + 1;
-
-
+% Use only samples with all 10 characters(10 '1' in multi_phenos,num=294),
+% and samples with no characters(10 '0' in multi_phenos,num=300).
+% Then the dimension of y is reduced from [1000*10] to [(294+300)*1].
+% Model in P2 can be used.
+y10 = find(multi_phenos_sum == 10); % [294*1]
+y0 = find(multi_phenos_sum == 0); % [300*1]
+y = [ones(length(y10),1);zeros(length(y0),1)];
+x = zeros(length(y10)+length(y0),num_sites*3);
+for i = 1 : length(y10)
+    x(i,:) = genotype_3x(y10(i),:);
+end
+for i = 1 : length(y0)
+   x(length(y10)+i,:) =  genotype_3x(y0(i),:);
 end
 
-%%
-% Remove repeated bits in possible_pathogenic_idx.
-possible_pathogenic_idx = ...
-    reshape(possible_pathogenic_idx,numel(possible_pathogenic_idx),1);
-possible_pathogenic_idx = sort(possible_pathogenic_idx,'ascend');
-possible_pathogenic_idx = fun_delete_duplicate(possible_pathogenic_idx);
-% Now there are 869 elements in possible_pathogenic_idx.
+[correlation,pval] = corr(x,y);
 
+[sorted_psb,sorted_psb_idx] = sort(pval,'ascend');
+
+possible_pathogenic_idx = sorted_psb_idx(1:num_psb_sites);
+possible_pathogenic_idx = sort(possible_pathogenic_idx,'ascend');
+sorted_psb_idx = floor((possible_pathogenic_idx - 1) / 3) + 1;
+% Remove duplicate sites.
+sorted_psb_idx = fun_delete_duplicate(sorted_psb_idx);
+
+save('p4_result','sorted_psb_idx')
+
+%%%%%%%%%%%%%%%%%%%%%%THE END%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+if 0
 %%
-Y = multi_phenos_sum.^2; % (Y(1)+...+Y(10))^2.
+Y = multi_phenos_sum;
+Y(find(multi_phenos_sum ~= 10 && multi_phenos_sum ~= 0)) = [];
 
 X = [];
 for i = 1 : length(possible_pathogenic_idx)
     X = [X, genotype_3x(:,possible_pathogenic_idx(i))];
 end
+X(find(multi_phenos_sum ~= 10 && multi_phenos_sum ~= 0,:)) = [];
 
 %%
 % B = regress(Y,X) returns the vector B of regression coefficients
@@ -97,6 +98,5 @@ end
 
 %%
 
-%TODO: give final results.
-
+end
 
